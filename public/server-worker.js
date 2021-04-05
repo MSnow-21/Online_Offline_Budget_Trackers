f4d9dfbf7f7d009876a301bff2ef329db50db313
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-04-05 12:16:00 -0400

    Created server-worker.js. Added files to cache array.

---
+++ public/server-worker.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/db.js",
+    "/index.js",
+    "/styles.css",
+    "/manifest.webmanifest",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+];
